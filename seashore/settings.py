28c056def1d463ec26aa0537f1fb68930c6822ae
ghostpete <ghostcoderme@gmail.com>
2026-02-06 10:20:44 +0100

    added remote db

--- seashore/settings.py
+++ seashore/settings.py
@@ -123,11 +123,18 @@ WSGI_APPLICATION = 'seashore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600
+    )
 }
 
 
